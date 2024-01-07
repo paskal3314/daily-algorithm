c20c6e0836aef5e6782fe08eaf00ca812e2a0431
Jin Lee <paskal3314@gmail.com>
2024-01-06 18:50:10 -0700

    Resolved: Problem 142

---
+++ Problems/Medium/142. Linked List Cycle II/mySolution.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  const arr = [];
+  let curr = head;
+  while (curr?.next) {
+    const index = arr.findIndex((el) => el === curr.next);
+    if (index === -1) {
+      arr.push(curr);
+      curr = curr.next;
+    } else {
+      return curr.next;
+    }
+  }
+  return null;
+};
+function run() {}
+
+run();
