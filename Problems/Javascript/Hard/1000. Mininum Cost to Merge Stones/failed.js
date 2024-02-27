1c933e040d392a3da1460a5cbe1703710e11510a
Jin Lee <paskal3314@gmail.com>
2024-02-26 22:39:29 -0700

    Failed: problem 1000

---
+++ Problems/Javascript/Hard/1000. Mininum Cost to Merge Stones/failed.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} stones
+ * @param {number} k
+ * @return {number}
+ */
+var mergeStones = function (stones, k) {
+  let cost = 0;
+  while (stones.length > k) {
+    let min = Number.MAX_SAFE_INTEGER;
+    let highest = 0;
+    let firstIndex = 0;
+    for (let i = 0; i < stones.length - k + 1; i++) {
+      let currMin = 0;
+      let currHighest = stones[i];
+      for (let j = i; j < k; j++) {
+        currMin += stones[j];
+        currHighest = Math.max(currHighest, stones[j]);
+      }
+      if (min === currMin && highest < currHighest) {
+        highest = currHighest;
+        firstIndex = i;
+      } else if (min > currMin) {
+        min = currMin;
+        highest = currHighest;
+        firstIndex = i;
+      }
+    }
+    cost += min;
+    stones.splice(firstIndex, k, min);
+  }
+  return stones.length > 1 ? -1 : cost;
+};
