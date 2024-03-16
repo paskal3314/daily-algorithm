f5176ffb1113bda358933d6b20c9aa4f8f704bf6
Jin Lee <paskal3314@gmail.com>
2024-03-15 21:47:44 -0600

    Learned: problem 863

---
+++ Problems/Javascript/Medium/863. All Nodes Distance K in Binary Tree/learned.js
@@ -0,0 +1,43 @@
+/**
+ * Beats 96%
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+var distanceK = function (root, target, k) {
+  if (!root) return [];
+  const node = findTarget(root, null, target);
+  const res = [];
+  findAllKApart(node, k, res);
+  return res;
+};
+
+function findTarget(root, parent, target) {
+  if (!root) return null;
+  root.parent = parent;
+  if (root === target) {
+    return root;
+  }
+  return findTarget(root.left, root, target) || findTarget(root.right, root, target);
+}
+
+function findAllKApart(root, k, res) {
+  if (!root || root.visited) return res;
+  if (k === 0) {
+    res.push(root.val);
+    return res;
+  }
+  root.visited = true;
+  findAllKApart(root.left, k - 1, res);
+  findAllKApart(root.right, k - 1, res);
+  findAllKApart(root.parent, k - 1, res);
+  return res;
+}
