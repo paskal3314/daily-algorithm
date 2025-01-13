0e138b0348ae2d64ffcaf3ea77f01b4b149dd611
Jin Lee <paskal3314@gmail.com>
2025-01-12 18:35:26 -0700

    Resolved: problem 189 - push n times of array

---
+++ Problems/Javascript/Medium/189. Rotate Array/resolved.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  const n = nums.length;
+  const num = k % n;
+  // console.log
+  const first = nums.slice(n - num);
+  const last = nums.slice(0, n - num);
+  console.log({ first, last });
+  for (let i = 0; i < first.length; i++) {
+    nums[i] = first[i];
+  }
+  for (let i = first.length; i < n; i++) {
+    nums[i] = last[i - first.length];
+  }
+};
