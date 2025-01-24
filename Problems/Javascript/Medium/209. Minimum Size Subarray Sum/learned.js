5bf40440572844ae5d43b5b8ba03f51e27b43dae
Jin Lee <paskal3314@gmail.com>
2025-01-23 23:06:21 -0700

    Learned: problem 209 - Minimum size subarray sum - sliding window

---
+++ Problems/Javascript/Medium/209. Minimum Size Subarray Sum/learned.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function (target, nums) {
+  let result = Infinity;
+  let left = 0;
+  let sum = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+
+    while (sum >= target) {
+      result = Math.min(result, right - left + 1);
+      sum -= nums[left++];
+    }
+  }
+  return result === Infinity ? 0 : result;
+};
