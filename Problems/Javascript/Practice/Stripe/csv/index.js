245b8c6a0fa7eb3f4fda96527edb8e547f9ba76c
Jin Lee <paskal3314@gmail.com>
2025-01-29 21:35:33 -0700

    Practiced: parse CSV

---
+++ Problems/Javascript/Practice/Stripe/csv/index.js
@@ -0,0 +1,30 @@
+function interpreteCSV(inputCSV) {
+  let [columns, ...rows] = csv.split("\n");
+  columns = columns.split(";").map((str) => str.trim());
+  rows = rows.map((str) => str.split(";").map((str) => str.trim()));
+
+  let result = [];
+  rows.forEach((infoArr) => {
+    const json = {};
+    for (let i = 0; i < columns.length; i++) {
+      const key = columns[i];
+      const value = infoArr[i];
+      json[key] = value;
+      //   console.log("[T]", {
+      //     key,
+      //     value,
+      //   });
+    }
+    result.push(json);
+  });
+  console.log("Result: ", result);
+}
+
+const csv = `Username; Identifier;First name;Last name
+booker12;9012;Rachel;Booker
+grey07;2070;Laura;Grey
+johnson81;4081;Craig;Johnson
+jenkins46;9346;Mary;Jenkins
+smith79;5079;Jamie;Smith;tt`;
+
+interpreteCSV(csv);
