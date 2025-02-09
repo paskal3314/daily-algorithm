8ed3bfbbab16eae4a69afdffc895164c7f604ebd
Jin Lee <paskal3314@gmail.com>
2025-02-09 15:08:08 -0700

    Resolved: Problem 1094 - car pooling - array

---
+++ Problems/Javascript/Medium/1094. Car Pooling/resolved.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} trips
+ * @param {number} capacity
+ * @return {boolean}
+ */
+var carPooling = function (trips, capacity) {
+  const locationMax = Math.max(...trips.map((el) => el[2]));
+  let hash = Array(locationMax + 1).fill(0);
+  for (const [numPassengers, from, to] of trips) {
+    for (let i = from; i < to; i++) {
+      hash[i] += numPassengers;
+      if (hash[i] > capacity) {
+        return false;
+      }
+    }
+  }
+  // console.log("[T]", hash)
+  return true;
+};
