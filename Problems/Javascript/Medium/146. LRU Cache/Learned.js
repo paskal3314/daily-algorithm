eb493bdb96d714b37db71d8b09275ce0ebeaa41d
Jin Lee <paskal3314@gmail.com>
2025-04-27 12:18:16 -0600

    Learned: problem 146 - LRU Cache Object

---
+++ Problems/Javascript/Medium/146. LRU Cache/Learned.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  this.capacity = capacity;
+  this.map = new Map();
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (!this.map.has(key)) return -1;
+
+  const value = this.map.get(key);
+  this.map.delete(key);
+  this.map.set(key, value);
+  return value;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  if (this.map.has(key)) {
+    this.map.delete(key);
+  }
+  this.map.set(key, value);
+  if (this.map.size > this.capacity) {
+    const oldestKey = this.map.keys().next().value;
+    this.map.delete(oldestKey);
+  }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
