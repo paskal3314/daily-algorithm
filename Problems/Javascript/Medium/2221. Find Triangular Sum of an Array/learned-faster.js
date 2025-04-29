0007b17958f89931c63fa1a3f65641ffd5621384
Jin Lee <paskal3314@gmail.com>
2025-04-28 23:43:06 -0600

    Learned: problem 2221 - triangular sum

---
+++ Problems/Javascript/Medium/2221. Find Triangular Sum of an Array/learned-faster.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangularSum = function (nums) {
+  let start = 0,
+    end = nums.length - 1;
+  while (start < end) {
+    for (let i = start; i < end; i++) {
+      nums[i] = (nums[i] + nums[i + 1]) % 10;
+    }
+    end--;
+  }
+  return nums[0];
+};
